fae36283e7e4787d0823d18fe4786c0e74870b72
Fawad Shah <52949387+fawadshah1@users.noreply.github.com>
2021-08-13 02:59:53 +0500

    removed old consoles

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,7 @@
 const app = require("./app");
 const connectDB = require("./config/database");
 
-if (
-  process.env.NODE_ENV !== "production" ||
-  process.env.NODE_ENV === "production"
-)
+if (process.env.NODE_ENV !== "production")
   require("dotenv").config({ path: "backend/config/config.env" });
 const PORT = process.env.PORT || 5000;
 
